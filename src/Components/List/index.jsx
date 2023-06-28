ba21c445cd7103c6f3cccc5ba27ba0c663340c83
KaedenOC <komeacarr2@gmail.com>
2023-06-28 13:20:45 -0700

    able to render list on other pages

--- src/Components/List/index.jsx
+++ src/Components/List/index.jsx
@@ -6,17 +6,17 @@ function List({ list, toggleComplete, deleteItem }) {
   //utilizing the useContext hook//We use the useContext hook to access the SettingsContext and destructure the displayItems value from the context.
   const { displayItems, showCompleted } = useContext(SettingsContext);
 
+  const [activePage, setPage] = useState(1);
   // Filter the list based on the showCompleted value
   const filteredList = showCompleted ? list : list.filter(item => !item.complete);
   //This ensures that only a maximum of displayItems items are included in the itemsToDisplay array.
-  const itemsToDisplay = filteredList.slice(0, displayItems);
+  // const itemsToDisplay = filteredList.slice(0, displayItems);
 
-  const [activePage, setPage] = useState(1);
   const pageCount = Math.ceil(filteredList.length / displayItems);
 
-  const firstItem = (activePage - 1) * displayItems;
-  const lastItem = activePage * displayItems;
-  const finalItems = itemsToDisplay.slice(firstItem, lastItem);
+  const listStart = displayItems * (activePage - 1);
+  const listEnd = listStart + displayItems;
+  const finalItems = filteredList.slice(listStart, listEnd);
   return (
     <>
       {finalItems.map(item => (
