1410789b3e44b2fa1cc1f1752a1933e4e0f30797
KaedenOC <komeacarr2@gmail.com>
2023-06-27 18:28:24 -0700

    showCompleted rendered as false unless clicked

--- src/Components/List/index.jsx
+++ src/Components/List/index.jsx
@@ -2,20 +2,25 @@ import React, { useContext } from 'react';
 import { SettingsContext } from '../../Context/Settings';
 
 function List({ list, toggleComplete, deleteItem }) {
-  const { displayItems } = useContext(SettingsContext);
-  const itemsToDisplay = list.slice(0, displayItems);
+  //utilizing the useContext hook//We use the useContext hook to access the SettingsContext and destructure the displayItems value from the context.
+  const { displayItems, showCompleted } = useContext(SettingsContext);
 
-  return(
+  // Filter the list based on the showCompleted value
+  const filteredList = showCompleted ? list : list.filter(item => !item.complete);
+  //This ensures that only a maximum of displayItems items are included in the itemsToDisplay array.
+  const itemsToDisplay = filteredList.slice(0, displayItems);
+
+  return (
     <>
-    {itemsToDisplay.map(item => (
-      <div key={item.id}>
-        <p>{item.text}</p>
-        <p><small>Assigned to: {item.assignee}</small></p>
-        <p><small>Difficulty: {item.difficulty}</small></p>
-        <div onClick={() => toggleComplete(item.id)}>Complete: {item.complete.toString()}</div>
-        <hr />
-      </div>
-    ))}
+      {itemsToDisplay.map(item => (
+        <div key={item.id}>
+          <p>{item.text}</p>
+          <p><small>Assigned to: {item.assignee}</small></p>
+          <p><small>Difficulty: {item.difficulty}</small></p>
+          <div onClick={() => toggleComplete(item.id)}>Complete: {item.complete.toString()}</div>
+          <hr />
+        </div>
+      ))}
     </>
   )
 }
