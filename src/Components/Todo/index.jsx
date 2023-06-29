c16fe6d728e7f94605c698f76947a68a206e5a6d
KaedenOC <komeacarr2@gmail.com>
2023-06-28 17:28:06 -0700

    todo styling

--- src/Components/Todo/index.jsx
+++ src/Components/Todo/index.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import useForm from '../../hooks/form';
 import List from '../List/index';
+import { SimpleGrid, Container, Stack, Paper, Text, Input, Button, Slider, Title } from '@mantine/core';
 
 import { v4 as uuid } from 'uuid';
 
@@ -50,32 +51,44 @@ const Todo = () => {
   return (
     <>
 
-      {/* leave the form code inside of the Todo Component */}
-      <form onSubmit={handleSubmit}>
+<Container>
 
-        <h2>Add To Do Item</h2>
+        {/* leave the form code inside of the Todo Component */}
+        <SimpleGrid cols={2} spacing="sm" verticalSpacing="lg">
+          <div>
+            <Paper padding="lg" radius="sm" withBorder p="md">
 
-        <label>
-          <span>To Do Item</span>
-          <input onChange={handleChange} name="text" type="text" placeholder="Item Details" />
-        </label>
+              <form onSubmit={handleSubmit}>
+                <Stack >
+                  <Title order={3}>Add To Do Item</Title>
 
-        <label>
-          <span>Assigned To</span>
-          <input onChange={handleChange} name="assignee" type="text" placeholder="Assignee Name" />
-        </label>
+                  <label>
+                    <Text fz="sm" fw={500}>To Do Item</Text>
+                    <Input size="sm" onChange={handleChange} name="text" type="text" placeholder="Item Details" />
+                  </label>
 
-        <label>
-          <span>Difficulty</span>
-          <input onChange={handleChange} defaultValue={defaultValues.difficulty} type="range" min={1} max={5} name="difficulty" />
-        </label>
+                  <label>
+                    <Text fz="sm" fw={500}>Assigned To</Text>
+                    <Input size="sm" onChange={handleChange} name="assignee" type="text" placeholder="Assignee Name" />
+                  </label>
 
-        <label>
-          <button type="submit">Add Item</button>
-        </label>
-      </form>
-      <List list={list} toggleComplete={toggleComplete} deleteItem={deleteItem}></List>
+                  <label>
+                    <Text fz="md">Difficulty</Text>
+                    <Slider onChange={handleChange} defaultValue={defaultValues.difficulty} type="range" min={1} max={5} name="difficulty" />
+                  </label>
 
+                  <label>
+                    <Button size="sm" type="submit">Add Item</Button>
+                  </label>
+                </Stack>
+              </form>
+            </Paper>
+          </div>
+          <div>
+            <List list={list} toggleComplete={toggleComplete} />
+          </div>
+        </SimpleGrid>
+      </Container>
     </>
   );
 };
